39e358c27b297b504c3b9ad4a0c480e41658e826
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-09-24 10:42:30 -0500

    give the 2nd try for solution

--- src/medium/Problem2594.java
+++ src/medium/Problem2594.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 public class Problem2594 {
 
 	private static long minValue = Long.MAX_VALUE;
-	
+
 	private static void printList(int[] a) {
 		var s = new StringBuilder();
 		for (int i = 0; i < a.length; i++) {
@@ -20,44 +20,61 @@ public class Problem2594 {
 		}
 		System.out.println(s.toString());
 	}
-	
+
 	public static long getMaxMinutes(int[] ranks, int[] assigned) {
 		long max = 0;
 		for (int i = 0; i < ranks.length; i++) {
 			long mins = ranks[i] * assigned[i] * assigned[i];
-			if (mins > max) max = mins;
+			if (mins > max)
+				max = mins;
 		}
 		return max;
 	}
-	
-	public static boolean isValid(int[] assigned, int cars) {
-		int total = 0;
-		for (int i = 0; i < assigned.length; i++) {
-			total += assigned[i];
-		}
-		return total == cars;
-	}
 
-	public static void Try(int[] ranks, int cars, int remainingCars, int[] assigned, int i) {
-//		long mins = calculateMinutes(ranks, assigned);
-//		if (mins > minValue) return;
-		if (i == ranks.length) {
-//			printList(assigned);
-			boolean isValid = isValid(assigned, cars);
-//			System.out.println(isValid);
-			if (!isValid) return;
+//	public static boolean isValid(int[] ranks, int cars, long mins) {
+//		long total = 0;
+//		if (total == mins)
+//			return true;
+//		return false;
+//	}
+
+	public static void process(int[] ranks, int cars, int[] assigned, int i) {
+		int noMachines = ranks.length;
+		int numMachineLeft = noMachines - (i + 1);
+		int remainingCars = cars;
+		if (i > 0) {
+			for (int j = i - 1; j >= 0; j--) {
+				remainingCars -= assigned[j];
+			}
+		}
+		if (i == noMachines - 1) {
+			assigned[i] = remainingCars;
 //			printList(assigned);
 			long mins = getMaxMinutes(ranks, assigned);
-//			System.out.println(mins + "\n ---");
+//			System.out.println(mins);
 			if (mins < minValue) {
 				minValue = mins;
 			}
 			return;
 		}
-		for (int c = remainingCars; c > 0; c--) {
+//		if (i == noMachines) {
+//			printList(assigned);
+//			long mins = getMaxMinutes(ranks, assigned);
+//			System.out.println(mins);
+//			if (mins < minValue && remainingCars == 0) {
+//				minValue = mins;
+//			}
+//			return;
+//		}
+		int avg = cars / noMachines;
+//		int start = Math.min(1, avg);
+		int start = avg;
+		if (i > 0)
+			start = 1;
+		int end = remainingCars - numMachineLeft; // possible cars can assign to rank ith
+		for (int c = end; c >= start; c--) {
 			assigned[i] = c;
-			int newRemainingCars = remainingCars - c;
-			Try(ranks, cars, newRemainingCars, assigned, i + 1);
+			process(ranks, cars, assigned, i + 1);
 		}
 	}
 
@@ -66,18 +83,18 @@ public class Problem2594 {
 
 		Arrays.sort(ranks);
 
-		var assigned = new int[ranks.length]; // assign assigned[i] cars for car ith;
+		var assigned = new int[ranks.length]; // assign assigned[i] cars for machine ith;
+		process(ranks, cars, assigned, 0);
 
-		Try(ranks, cars, cars, assigned, 0);
-		
 //		System.out.println(minValue);
 
 		return minValue;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(repairCars(new int[] { 4, 2, 3, 1 }, 10));
-		System.out.println(repairCars(new int[] { 5, 1, 8 }, 6));
+//		System.out.println(repairCars(new int[] { 4, 2, 3, 1 }, 10));
+//		System.out.println(repairCars(new int[] { 5, 1, 8 }, 6));
+		System.out.println(repairCars(new int[] { 3, 3, 1, 2, 1, 1, 3, 2, 1 }, 58));
 	}
 
 }
