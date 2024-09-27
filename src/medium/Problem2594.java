72415bdee888efbc1719fa958a4a84da2ba2b261
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-09-26 21:04:32 -0500

    give the 3rd try for solution - greedy - no exact answers

--- src/medium/Problem2594.java
+++ src/medium/Problem2594.java
@@ -1,7 +1,6 @@
 package medium;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 /*
  * 1 <= ranks.length <= 10^5
@@ -10,8 +9,6 @@ import java.util.stream.Collectors;
  */
 
 public class Problem2594 {
-
-	private static long minValue = Long.MAX_VALUE;
 
 	private static void printList(int[] a) {
 		var s = new StringBuilder();
@@ -31,64 +28,24 @@ public class Problem2594 {
 		return max;
 	}
 
-//	public static boolean isValid(int[] ranks, int cars, long mins) {
-//		long total = 0;
-//		if (total == mins)
-//			return true;
-//		return false;
-//	}
-
-	public static void process(int[] ranks, int cars, int[] assigned, int i) {
-		int noMachines = ranks.length;
-		int numMachineLeft = noMachines - (i + 1);
-		int remainingCars = cars;
-		if (i > 0) {
-			for (int j = i - 1; j >= 0; j--) {
-				remainingCars -= assigned[j];
-			}
-		}
-		if (i == noMachines - 1) {
-			assigned[i] = remainingCars;
-//			printList(assigned);
-			long mins = getMaxMinutes(ranks, assigned);
-//			System.out.println(mins);
-			if (mins < minValue) {
-				minValue = mins;
-			}
-			return;
-		}
-//		if (i == noMachines) {
-//			printList(assigned);
-//			long mins = getMaxMinutes(ranks, assigned);
-//			System.out.println(mins);
-//			if (mins < minValue && remainingCars == 0) {
-//				minValue = mins;
-//			}
-//			return;
-//		}
-		int avg = cars / noMachines;
-//		int start = Math.min(1, avg);
-		int start = avg;
-		if (i > 0)
-			start = 1;
-		int end = remainingCars - numMachineLeft; // possible cars can assign to rank ith
-		for (int c = end; c >= start; c--) {
-			assigned[i] = c;
-			process(ranks, cars, assigned, i + 1);
-		}
-	}
-
 	// https://leetcode.com/problems/minimum-time-to-repair-cars/description/
 	public static long repairCars(int[] ranks, int cars) {
 
 		Arrays.sort(ranks);
 
 		var assigned = new int[ranks.length]; // assign assigned[i] cars for machine ith;
-		process(ranks, cars, assigned, 0);
+		int remainCars = cars;
+		int avg = cars / ranks.length;
+		for (int i = ranks.length - 1; i > 0; i--) {
+			assigned[i] = avg;
+			remainCars -= avg;
+		}
+		assigned[0] = remainCars;
+		long result = getMaxMinutes(ranks, assigned);
 
-//		System.out.println(minValue);
+		printList(assigned);
 
-		return minValue;
+		return result;
 	}
 
 	public static void main(String[] args) {
