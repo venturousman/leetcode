63891ec4859af06679fb179c4f6f31bf42a766c6
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-10-05 23:38:20 -0500

    update next permutation

--- src/nextgeneration/Permutation.java
+++ src/nextgeneration/Permutation.java
@@ -14,12 +14,7 @@ public class Permutation {
 	public static void reverse(int a[], int from, int to) {
 		int b = from, e = to;
 		while (b < e) {
-//			swap(a[b], a[e]);
 			swap(a, b, e);
-//			int temp = a[b];
-//			a[b] = a[e];
-//			a[e] = temp;
-
 			b++;
 			e--;
 		}
@@ -27,48 +22,47 @@ public class Permutation {
 
 	// Liet ke hoan vi
 	public static boolean nextGeneration(int b[], int n) {
-		int i = n - 1;
-		while (i > 0 && b[i] > b[i + 1])
-			i--;
-		if (i == 0)
+
+		// If the given dataset is empty or contains only one element next_permutation
+		// is not possible
+		if (n <= 1)
 			return false;
 
-		int k = n;
-		while (b[k] < b[i])
+		int i = n - 2;
+		// Find the first pair from the right where nums[i] < nums[i + 1]
+		while (i >= 0 && b[i] > b[i + 1])
+			i--;
+		if (i < 0)
+			return false;
+
+		int k = n - 1;
+		// Find the first element from the right greater than nums[i]
+		while (k >= 0 && b[k] < b[i])
 			k--;
 
 		// if(k==i) return 0;
 
-//		HoanVi(b[k], b[i]);
 		swap(b, k, i);
-		// swap
-//		int temp = b[k];
-//		b[k] = b[i];
-//		b[i] = temp;
-
-		reverse(b, i + 1, n);
-
+		reverse(b, i + 1, n - 1);
 		return true;
 	}
 
 	public static void printList(int[] a, int n) {
-		for (int i = 1; i <= n; i++)
+		for (int i = 0; i < n; i++)
 			System.out.print(a[i] + " ");
 		System.out.println();
 	}
 
 	public static void main(String[] args) {
-		int n = 4, max = 100;
+		int n = 3, max = 100;
 		int[] a = new int[max + 1];
 
-		for (int i = 1; i <= n; i++)
-			a[i] = i;
+		for (int i = 0; i < n; i++)
+			a[i] = i + 1;
 
-		printList(a, n);
-
-		while (nextGeneration(a, n)) {
+		do {
 			printList(a, n);
-		}
+		} while (nextGeneration(a, n));
 	}
 
 }
