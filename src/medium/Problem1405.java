e2e64bf4bdf46ce924a79c770b4ef636348311aa
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2024-10-16 20:13:34 -0500

    commit solution for problem 1405 - greedy 2 - wrong answer

--- src/medium/Problem1405.java
+++ src/medium/Problem1405.java
@@ -2,95 +2,41 @@ package medium;
 
 public class Problem1405 {
 
-	public static String longestDiverseString(int a, int b, int c) {
+	public static String longestDiverseString(StringBuilder x, StringBuilder y, StringBuilder z) {
 		var s = new StringBuilder();
-		// init
-		if (a >= b && a >= c) {
-			for (int i = 0; i < a; i++) {
-				s.append('a');
-			}
-			if (b >= c) {
-				for (int i = 0; i < b; i++) {
-					s.append('b');
-				}
-				for (int i = 0; i < c; i++) {
-					s.append('c');
-				}
-			} else {
-				for (int i = 0; i < c; i++) {
-					s.append('c');
-				}
-				for (int i = 0; i < b; i++) {
-					s.append('b');
-				}
-			}
-		} else if (b >= a && b >= c) {
-			for (int i = 0; i < b; i++) {
-				s.append('b');
-			}
-			if (a >= c) {
-				for (int i = 0; i < a; i++) {
-					s.append('a');
-				}
-				for (int i = 0; i < c; i++) {
-					s.append('c');
-				}
-			} else {
-				for (int i = 0; i < c; i++) {
-					s.append('c');
-				}
-				for (int i = 0; i < a; i++) {
-					s.append('a');
-				}
-			}
-		} else if (c >= a && c >= b) {
-			for (int i = 0; i < c; i++) {
-				s.append('c');
-			}
-			if (a >= b) {
-				for (int i = 0; i < a; i++) {
-					s.append('a');
-				}
-				for (int i = 0; i < b; i++) {
-					s.append('b');
-				}
-			} else {
-				for (int i = 0; i < b; i++) {
-					s.append('b');
-				}
-				for (int i = 0; i < a; i++) {
-					s.append('a');
-				}
-			}
-		}
-//		System.out.println(s.toString());
 
-		if (s.length() < 3) {
-			return s.toString();
-		} else if (s.length() == 3) {
-			if (s.charAt(0) == s.charAt(1) && s.charAt(0) == s.charAt(2)) {
-				s.deleteCharAt(2);
-			}
-			return s.toString();
-		} else {
-			// s.length() > 3
-			int i = 2;
-			while (i < s.length()) {
-				if (s.charAt(i) == s.charAt(i - 1) && s.charAt(i) == s.charAt(i - 2)) {
-					int k = i + 1;
-					while (k < s.length() && s.charAt(k) == s.charAt(i))
-						k++;
-					if (k < s.length()) {
-						// swap
-						var x = s.charAt(i);
-						s.setCharAt(i, s.charAt(k));
-						s.setCharAt(k, x);
-					} else if (k == s.length()) {
-//						s.deleteCharAt(i);
-						s.delete(i, k);
-					}
+		while (x.length() > 0 || y.length() > 0 || z.length() > 0) {
+			if (s.length() == 0 || (s.length() > 0 && x.length() > 0 && s.charAt(s.length() - 1) != x.charAt(0))) {
+				int size = Math.min(2, x.length());
+				for (int i = 0; i < size; i++) {
+					s.append(x.charAt(0));
+					x.deleteCharAt(0);
 				}
-				i += 1;
+			} else {
+				if (x.length() > 0)
+					x.deleteCharAt(0);
+			}
+
+			if (s.length() == 0 || (s.length() > 0 && y.length() > 0 && s.charAt(s.length() - 1) != y.charAt(0))) {
+				int size = Math.min(2, y.length());
+				for (int i = 0; i < size; i++) {
+					s.append(y.charAt(0));
+					y.deleteCharAt(0);
+				}
+			} else {
+				if (y.length() > 0)
+					y.deleteCharAt(0);
+			}
+
+			if (s.length() == 0 || (s.length() > 0 && z.length() > 0 && s.charAt(s.length() - 1) != z.charAt(0))) {
+				int size = Math.min(2, z.length());
+				for (int i = 0; i < size; i++) {
+					s.append(z.charAt(0));
+					z.deleteCharAt(0);
+				}
+			} else {
+				if (z.length() > 0)
+					z.deleteCharAt(0);
 			}
 		}
 
@@ -99,11 +45,78 @@ public class Problem1405 {
 		return res;
 	}
 
+	public static String longestDiverseString(int a, int b, int c) {
+		var x = new StringBuilder();
+		var y = new StringBuilder();
+		var z = new StringBuilder();
+
+		if (a >= b && a >= c) {
+			for (int i = 0; i < a; i++) {
+				x.append('a');
+			}
+			if (b >= c) {
+				for (int i = 0; i < b; i++) {
+					y.append('b');
+				}
+				for (int i = 0; i < c; i++) {
+					z.append('c');
+				}
+			} else {
+				for (int i = 0; i < c; i++) {
+					y.append('c');
+				}
+				for (int i = 0; i < b; i++) {
+					z.append('b');
+				}
+			}
+		} else if (b >= a && b >= c) {
+			for (int i = 0; i < b; i++) {
+				x.append('b');
+			}
+			if (a >= c) {
+				for (int i = 0; i < a; i++) {
+					y.append('a');
+				}
+				for (int i = 0; i < c; i++) {
+					z.append('c');
+				}
+			} else {
+				for (int i = 0; i < c; i++) {
+					y.append('c');
+				}
+				for (int i = 0; i < a; i++) {
+					z.append('a');
+				}
+			}
+		} else if (c >= a && c >= b) {
+			for (int i = 0; i < c; i++) {
+				x.append('c');
+			}
+			if (a >= b) {
+				for (int i = 0; i < a; i++) {
+					y.append('a');
+				}
+				for (int i = 0; i < b; i++) {
+					z.append('b');
+				}
+			} else {
+				for (int i = 0; i < b; i++) {
+					y.append('b');
+				}
+				for (int i = 0; i < a; i++) {
+					z.append('a');
+				}
+			}
+		}
+
+		return longestDiverseString(x, y, z);
+	}
+
 	public static void main(String[] args) {
-//		System.out.println(longestDiverseString(1, 1, 7));
-//		System.out.println(longestDiverseString(7, 1, 0));
+		System.out.println(longestDiverseString(1, 1, 7)); // ccaccbcc
+//		System.out.println(longestDiverseString(7, 1, 0)); // aabaa
 //		System.out.println(longestDiverseString(2, 2, 1)); // aabbc
-		System.out.println(longestDiverseString(5, 4, 3)); // output: aabaababbcc - expected: aabbaaccbbac
+//		System.out.println(longestDiverseString(5, 4, 3)); // output: aabaababbcc - expected: aabbaaccbbac
 	}
 
 }
